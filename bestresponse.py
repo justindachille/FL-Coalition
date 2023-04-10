660891f2014b7bac89ff524cde53be14ac520e02
justin <justindachille@gmail.com>
2023-04-09 22:17:26 -0700

    add lambda for optimization

--- bestresponse.py
+++ bestresponse.py
@@ -1,9 +1,10 @@
 import jax.numpy as jnp
 from jax import grad
+import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
-theta_max = 1000
-A = [0.8, 0.7, 0.9]
+theta_max = 10
+A = [0.9, 0.8, 0.7]
 C_pri = [0.2, 0.3, 0.1]
 
 def sigma(m, n, p):
@@ -38,25 +39,38 @@ def W3Obj(p):
 def update_price(i, p):
     if i == 0:
         dW_dp = grad(W1Obj)
-        res = minimize(W1Obj, x0=p, jac=dW_dp, method='BFGS')
-        return res.x
+        res = minimize(lambda x: W1([x, p[1], p[2]]), jac=dW_dp, x0=p[0], method='Powell')
+        print(res)
+        return [res.x[0], p[1], p[2]]
     elif i == 1:
         dW_dp = grad(W2Obj)
-        res = minimize(W2Obj, x0=p, jac=dW_dp, method='BFGS')
-        return res.x
+        res = minimize(lambda x: W2([p[0], x, p[2]]), jac=dW_dp, x0=p[1], method='Powell')
+        print(res)
+        return [p[0], res.x[0], p[2]]
     elif i == 2:
         dW_dp = grad(W3Obj)
-        res = minimize(W3Obj, x0=p, jac=dW_dp, method='BFGS')
-        return res.x
+        res = minimize(lambda x: W3([p[0], p[1], x]), jac=dW_dp, x0=p[2], method='Powell')
+        print(res)
+        return [p[0], p[1], res.x[0]]
 
-p_init = [0, 0, 0]
+p_init = [0.5, 0.5, 0.5]
 p_new = p_init.copy()
 
 while True:
+    # test_values = [[p1, p2, p3] for p1 in range(1, 11, 5) for p2 in range(1, 11, 5) for p3 in range(1, 11, 5)]
+
+    # for p in test_values:
+    #     dW_dp = grad(W1Obj, allow_int=True)
+    #     w1_value = dW_dp(p)
+    #     print(f"p = {p}, W1 = {w1_value}")
+    # create 100 evenly spaced values from 0 to 10
+
+    ctr = 0
     for i in range(3):
-        p_new= update_price(float(i), p_new)
+        p_new = update_price(float(i), p_new)
         print(f'i: {i}, pnew: {p_new}')
-    if jnp.allclose(jnp.array(p_init), jnp.array(p_new), rtol=1e-3):
+        ctr += 1
+    if jnp.allclose(jnp.array(p_init), jnp.array(p_new), rtol=1e-3) or ctr > 100:
         break
     p_init = p_new.copy()
 
