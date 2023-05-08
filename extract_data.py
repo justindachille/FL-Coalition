8312196918dc5a9aab8f5481c19d6b36e2f68b20
justin-ucdavis <jtdachille@ucdavis.edu>
2023-05-07 22:00:40 -0700

    Changes

--- extract_data.py
+++ extract_data.py
@@ -166,9 +166,9 @@ def parse_ft_logs(coalitions):
                 #     print('after:', coalitions[key])
     return coalitions
 
-def generate_coalition_table(coalition, filename):
+def generate_coalition_table(i, coalition, filename, theta_max, is_uniform=True, mean=1, sd=1):
     table_header = ['Coalition structure', "Client A's accuracy", "Client B's accuracy", "Client C's accuracy"]
-    accuracies_as_table, reordered_profits, reordered_prices, profit_stability_dict, accuracy_stability_dict = createTableFromCoalition(single_coalition, 10000, True)
+    accuracies_as_table, reordered_profits, reordered_prices, profit_stability_dict, accuracy_stability_dict = createTableFromCoalition(coalition, theta_max, is_uniform=is_uniform, mean=mean, sd=sd)
     table_data = [
         (r"$\{A,B,C\}$", f"{coalition.ABC[0]*100:.2f}\\%", f"{coalition.ABC[1]*100:.2f}\\%", f"{coalition.ABC[2]*100:.2f}\\%"),
         (r"$\{A,B\}, \{C\}$", f"{coalition.AB_C[0]*100:.2f}\\%", f"{coalition.AB_C[1]*100:.2f}\\%", f"{coalition.AB_C[2]*100:.2f}\\%"),
@@ -176,7 +176,17 @@ def generate_coalition_table(coalition, filename):
         (r"$\{B,C\}, \{A\}$", f"{coalition.A_BC[0]*100:.2f}\\%", f"{coalition.A_BC[1]*100:.2f}\\%", f"{coalition.A_BC[2]*100:.2f}\\%"),
         (r"$\{A\}, \{B\}, \{C\}$", f"{coalition.A_B_C_[0]*100:.2f}\\%", f"{coalition.A_B_C_[1]*100:.2f}\\%", f"{coalition.A_B_C_[2]*100:.2f}\\%")
     ]
-    table = "\\begin{table}[h]\n\\centering\n\\caption{Training results.}\n\\label{training-results}\n\\begin{tabular}{|c|c|c|c|}\\hline\n"
+    uniform_str = "True" if is_uniform else "False"
+    table = (r"\subsection{Scenario " + str(i+1) + "}\n\n"
+             r"\textbf{Simulation Setup}: \n"
+             r"C Dataset Size: {" + str(coalition.C_size) + "}\n"
+             r"Dirichlet Beta Parameter: {" + str(coalition.beta) + "}\n"
+             r"Theta Max: {" + str(theta_max) + "}\n"
+             r"Is Uniform: {" + uniform_str + "}\n"
+             r"Mean: {" + str(mean) + "}\n"
+             r"SD: {" + str(sd) + "}\n\n"
+             r"\textbf{Numerical results}: \n\n")
+    table += "\\begin{table}[h]\n\\centering\n\\caption{Training results.}\n\\label{training-results}\n\\begin{tabular}{|c|c|c|c|}\\hline\n"
     table += ' & '.join(table_header) + '\\\\ \\hline\n'
     for row in table_data:
         table += ' & '.join([str(cell) for cell in row]) + '\\\\ \\hline\n'
@@ -234,6 +244,12 @@ if __name__ == '__main__':
     for k, v in coalition_str_dict.items():
         print(k, v)
 
-    single_coalition = coalitions[(2000, '0.1')]
-    print('single: ', single_coalition)
-    generate_coalition_table(single_coalition, '2000Coalition.txt')
+    for i, coalition in enumerate(coalitions):
+        generate_coalition_table(
+            i,
+            coalitions[coalition],
+            f'{coalition[0]}_Coalition.txt',
+            theta_max = 10000,
+            is_uniform=True,
+            mean=1,
+            sd=1,)
